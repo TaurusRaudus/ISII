d9105b612ff6432bfef8ca4ffede520426db9fb4
Manuel <jacintopichimawido27@gmail.com>
2025-04-02 11:00:47 -0500

    Create server.js

---
+++ Homework/Tools/Heroku/server.js
@@ -0,0 +1,19 @@
+const express = require("express"); // include
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));  // IMPORTANTE PONER BIEN EL NOMBRE HSHDHDS
+app.get("/",(req, res) => { res.sendFile(__dirname + "/public/index.html"); } );
+
+app.post("/process", (req, res) => {
+  const userInput = req.body.userInput;
+  res.send(`<p> enviado: ${userInput}</p>`); // Backticks para que se interprete como variable
+});
+
+// Iniciamos el servidor //////////////////////////////////////////////
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+//////////////////////////////////////////////////////////////////////
