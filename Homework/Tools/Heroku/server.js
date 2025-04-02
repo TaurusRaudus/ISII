5fa07c05c4225137f65b48c225a128a7ca203a92
Manuel <jacintopichimawido27@gmail.com>
2025-04-02 11:07:13 -0500

    Update server.js

--- Homework/Tools/Heroku/server.js
+++ Homework/Tools/Heroku/server.js
@@ -3,9 +3,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));  // IMPORTANTE PONER BIEN EL NOMBRE HSHDHDS
-app.get("/",(req, res) => { res.sendFile(__dirname + "/public/index.html"); } );
+app.get("/",(req, res) => { res.sendFile(__dirname + "/public/index.html"); } ); // Esto nos devuelve el folder a publc
 
 app.post("/process", (req, res) => {
   const userInput = req.body.userInput;
